7efe07dd284c17cc64dedc6b5a095a38ca62f4c6
sp0008 <saipriyagr17@gmail.com>
2022-08-12 16:50:26 +0530

    first commit

---
+++ OneToOneAssociation/src/com/cg/one/Address.java
@@ -0,0 +1,62 @@
+package com.cg.one;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Column;
+
+
+@Entity
+public class Address implements Serializable{
+	
+private static final long serialVersionUID=1L;
+@Id
+@GeneratedValue(strategy=GenerationType.AUTO)
+private int address_id;
+
+@Column(name="address_street")
+private String street;
+@Column(name="address_city")
+private String city;
+@Column(name="address_state")
+private String State;
+@Column(name="address_zipcode")
+private String zipcode;
+
+public int getAddress_id() {
+	return address_id;
+}
+public void setAddress_id(int address_id) {
+	this.address_id = address_id;
+}
+public String getStreet() {
+	return street;
+}
+public void setStreet(String street) {
+	this.street = street;
+}
+public String getCity() {
+	return city;
+}
+public void setCity(String city) {
+	this.city = city;
+}
+public String getState() {
+	return State;
+}
+public void setState(String state) {
+	State = state;
+}
+public String getZipcode() {
+	return zipcode;
+}
+public void setZipcode(String zipcode) {
+	this.zipcode = zipcode;
+}
+public static long getSerialversionuid() {
+	return serialVersionUID;
+}
+}
